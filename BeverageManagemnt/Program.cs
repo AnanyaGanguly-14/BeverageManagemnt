0ca274e9eb0e7368ee22b5b8e8df0bff04617a6c
AnanyaG14 <ananya.gly14@gmail.com>
2025-06-26 01:43:57 +0530

    Addde CorsPolicy

--- BeverageManagemnt/Program.cs
+++ BeverageManagemnt/Program.cs
@@ -26,6 +26,14 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddHttpContextAccessor();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+    policy =>
+    {
+        policy.AllowAnyOrigin() ;
+    });
+});
 
 var app = builder.Build();
 
@@ -41,5 +49,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseCors("AllowReactApp");
 
 app.Run();
